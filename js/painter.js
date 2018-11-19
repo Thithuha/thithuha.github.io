1dda1404601595ee29957894ddf93ba8a10178fb
Thithuha <chloe.nguyen1910@gmail.com>
2018-11-19 02:39:38 +0100

    Update painter.js

--- js/painter.js
+++ js/painter.js
@@ -28,32 +28,4 @@ document.addEventListener("mousemove", function (event) {
   }
 })
 
-const canvas = document.querySelector("canvas")
 
-canvas.width = window.innerWidth
-canvas.height = window.innerHeight
-
-const context = canvas.getContext("2d")
-
-context.strokeStyle = "#00336c"
-context.lineWidth = 10
-context.lineCap = "round"
-
-let shouldPaint = false
-
-document.addEventListener("mousedown", function (event) {
-  shouldPaint = true
-  context.moveTo(event.pageX, event.pageY)
-  context.beginPath()
-})
-
-document.addEventListener("mouseup", function (event) {
-  shouldPaint = false
-})
-
-document.addEventListener("mousemove", function (event) {
-  if (shouldPaint){
-  context.lineTo(event.pageX, event.pageY)
-  context.stroke()
-  }
-})
